2a626fd7f79e56b7275b7cb0942ab4d8db45a5f6
erattini <emiliano.rattini@studio.unibo.it>
2025-05-05 12:01:13 +0200

    Add javadoc

--- src/main/java/Clazz.java
+++ src/main/java/Clazz.java
@@ -1,9 +1,15 @@
 public class Clazz {
 
+    /**
+     * Clazz
+     */
     public Clazz() {
         print();
     }
 
+    /**
+     * print
+     */
     public void print() {
         System.out.println("Hello world!");
     }
